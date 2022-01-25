7b611ea0294421d5c00a89989cc98bcc11a66a7c
Vinicios Ragazzi <viniragazzzi@gmail.com>
2022-01-25 13:26:15 -0300

    Slide top Done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,42 +1,67 @@
-const cards = document.querySelector('.cards');
-const seta = document.getElementById('seta-scroll')
-let scrollPerClick;
-let imgPadding = 20;
+// const cards = document.querySelector('.cards');
+// const seta = document.getElementById('seta-scroll')
+// let scrollPerClick;
+// let imgPadding = 20;
 
-console.log('on')
+// console.log('on')
 
-let scrollAmount = 0;
+// let scrollAmount = 0;
 
-seta.addEventListener('click', sliderScrollRight)
+// seta.addEventListener('click', sliderScrollRight)
 
-function sliderScrollRight(){
-    console.log(cards.scrollWidth - cards.clientWidth)
-    scrollAmount+=455
-    if(scrollAmount <= (cards.scrollWidth - cards.clientWidth+500)){
-        cards.scrollTo({
-            top:0,
-            left: (scrollAmount),
-            behavior: "smooth",
-        });
-    }else{
-        cards.scrollTo({
-            top:0,
-            left: (0),
-            behavior: 'smooth'
-        });
-        scrollAmount=0
-    }
-    console.log(scrollAmount)
-}
+// function sliderScrollRight(){
+//     console.log(cards.scrollWidth - cards.clientWidth)
+//     scrollAmount+=455
+//     if(scrollAmount <= (cards.scrollWidth - cards.clientWidth+500)){
+//         cards.scrollTo({
+//             top:0,
+//             left: (scrollAmount),
+//             behavior: "smooth",
+//         });
+//     }else{
+//         cards.scrollTo({
+//             top:0,
+//             left: (0),
+//             behavior: 'smooth'
+//         });
+//         scrollAmount=0
+//     }
+//     console.log(scrollAmount)
+// }
 
 
 
-function sliderScrollLeft(){
-    cards.scrollTo({
-        top:0,
-        left: (scrollAmount -= scrollPerClick),
-        behavior: 'smooth'
-    });
+// function sliderScrollLeft(){
+//     cards.scrollTo({
+//         top:0,
+//         left: (scrollAmount -= scrollPerClick),
+//         behavior: 'smooth'
+//     });
    
 
-  }
+//   }
+
+
+let swiperCards = new Swiper("#cards-track", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ 
+    breakpoints: {
+        544: {
+            slidesPerView:1,
+            spaceBetween: 500, 
+        },
+        768: {
+          slidesPerView:3,
+          spaceBetween: 30,
+        },
+        1441: {
+            slidesPerView:4,
+            spaceBetween: 30,
+          },
+      },
+});
