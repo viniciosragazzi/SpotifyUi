87e82e4dbd0bfe1de0e93f754c188c0f90d5e4ef
Vinicios Ragazzi <viniragazzzi@gmail.com>
2022-01-27 21:48:09 -0300

    Sistema de Login Pronto

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -122,6 +122,7 @@ const addMusic = (musicas) => {
     </div>`;
 
     document.querySelector("#musics-card").appendChild(music);
+    console.log('adicionado')
   });
 };
 addMusic(musicas);
@@ -205,3 +206,218 @@ const mostrarAmigos = (amigos) => {
 };
 
 mostrarAmigos(amigos);
+
+
+// Area Player Bottom 
+function gerarPlayer(musica, musicas){
+  document.getElementById('player-bottom').innerHTML=
+  `
+  <div class="player-bottom_music">
+  <div class="img-music">
+      <img src="./assets/medias/img/img-musics/${musicas[musica].img}" alt="music-banner">
+  </div>
+
+      <div class="info">
+          <p class="nome-music text-lg">${musicas[musica].nome}</p>
+          <p class="nome-autor text-lg">${musicas[musica].cantor}</p>
+      </div>
+      <img class="heart-fav" src="./assets/medias/img/img-musics/heart (2).svg" alt="heart-2">
+</div>
+<div class="player-bottom_controls">
+  <div class="controls">
+      <img src="./assets/medias/img/bottomPlayerImg/skip-back.svg" id="back-button" alt="back-button">
+      <img src="./assets/medias/img/bottomPlayerImg/play.svg" id="play-button" alt="play-button">
+      <img src="./assets/medias/img/bottomPlayerImg/skip-back-1.svg" id="next-button" alt="next-button">
+  </div>
+  <div class="music-progressBar">
+      <span class="currentValueMusic">1:25</span>
+      <div class="progressArea">
+          <div class="progressBar"></div>
+          <div class="CurrentprogressBar"></div>
+      </div>
+      <span class="totalValueMusic">3:00</span>
+  </div>
+  <div class="volume-bar">
+      <img src="./assets/medias/img/bottomPlayerImg/volume-up.svg" alt="volume-up">
+
+      <div class="progressArea">
+          <div class="progressBar"></div>
+          <div class="CurrentprogressBar"></div>
+      </div>
+  </div>
+  <div class="controls-2">
+      <img src="./assets/medias/img/bottomPlayerImg/Repeat.svg" alt="Repeat">
+      <img src="./assets/medias/img/bottomPlayerImg/Aleatori.svg" alt="Aleatori">
+      <img src="./assets/medias/img/bottomPlayerImg/Vector.svg" alt="Vector">
+  </div>
+</div>
+</div>
+  `
+  
+}
+
+
+function ativarPlayer(musicas){
+  const musicasDom = document.querySelectorAll('div.music-area');
+  musicasDom.forEach((musica, index) => {
+    
+   const btnPlay = document.querySelector(`#${musica.id}>.buttons-musics>.play`)
+   btnPlay.addEventListener('click', ()=>{
+    const player = document.querySelector('#player-bottom')
+      
+    if(player.classList.contains('abrir')){
+      player.classList.remove('abrir')
+    }else{
+      player.classList.add('abrir')
+
+      const musicSplit = musica.id.split("-", 2)
+      gerarPlayer(musicSplit[1], musicas)
+    }
+    if(btnPlay.classList.contains('pause')){
+      btnPlay.setAttribute('src', './assets/medias/img/img-musics/Play (2).svg')
+      btnPlay.classList.remove('pause')
+    }else{
+      btnPlay.setAttribute('src', './assets/medias/img/img-musics/pause.svg')
+      btnPlay.classList.add('pause')
+    }
+    
+
+    })
+  })
+}
+
+ativarPlayer(musicas)
+
+
+
+//================LIMPA O CAMPO DO MODAL =======================
+const limparCampo = () => {
+  const fields = document.querySelectorAll(".field-reg");
+  const fields2 = document.querySelectorAll(".field-login");
+  fields2.forEach((field) => (field.value = ""));
+  fields.forEach((field) => (field.value = ""));
+};
+
+  //================PERMITE PEGAR TODOS OS DADOS DE UM DETERMINADO BD DO LOCAL STORAGE =======================
+  const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("dbCliente")) ?? [];
+  
+  
+  
+  //================PERMITE ADICIONAR  DADOS A UM DETERMINADO BD DO LOCAL STORAGE =======================
+  const setLocalStorage = (dbCliente) =>
+    localStorage.setItem("dbCliente", JSON.stringify(dbCliente));
+  
+  
+    //================LE TODOS OS CLIENTES E NO LOCAL STORAGE =======================
+  
+  const readClient = () => getLocalStorage();
+  
+  //================CRIA UM CLIENTE E SALVA NA LOCAL STORAGE =======================
+  const createClient = (client) => {
+    const dbCliente = getLocalStorage();
+    dbCliente.push(client);
+    setLocalStorage(dbCliente);
+  };
+  const isValidCadastro = () => {
+    return document.getElementById("form-register").reportValidity();
+  };
+  const saveClient = () => {
+    if (isValidCadastro()) {
+      const client = {
+        nome: document.getElementById("nameRegister").value,
+        username: document.getElementById("usernameRegister").value,
+        email: document.getElementById("emailRegister").value,
+        password: document.getElementById("passwordRegister").value,
+      };
+      createClient(client);
+      limparCampo();
+      document.location.reload(true);
+    }
+  };
+  
+  const isValidLogin= () => {
+    return document.getElementById("form-login").reportValidity();
+  };
+const validarLogin = () => {
+  if(isValidLogin()){
+    const userAtual = document.getElementById('usernameLogin').value
+    const passAtual = document.getElementById('passwordLogin').value
+   
+    const contas = readClient()
+    let r = []
+    contas.forEach((conta, index)=>{
+
+  
+        if(userAtual != undefined && passAtual != undefined){
+          if(userAtual == conta.username && passAtual == conta.password ){
+            r = [{id:index, conta: conta, valido: true}]
+          }
+        }
+    })
+    return r
+  }
+
+
+}
+
+
+
+//Tela de Login Area
+const controlTelaLogin = () => {
+  const telaLogin = document.querySelector('#telaLogin');
+  const body = document.querySelector('body');
+  const telaPrincipal = document.querySelector('#all-page');
+  const btnEntrar = document.querySelector('#entrarLogin');
+
+
+
+
+  btnEntrar.addEventListener('click', () => {
+    if(validarLogin().length == 1){
+      logar()
+    }else{
+      limparCampo()
+      document.querySelector('#aviso-invalido').style.display = 'flex'
+    }
+    
+  })
+
+  const logar = () =>{
+      telaLogin.style.display = 'none'
+      telaPrincipal.style.display = 'flex'
+      document.querySelector('#title-top').innerHTML= `Playlist for ${validarLogin()[0].conta.nome} `
+      telaPrincipal.style.opacity = '1'
+      body.style.overflowY = 'scroll';
+  }
+
+  const boxFormAnimation = () =>{
+    const loginBtn = document.querySelector('.toRegister');
+    const registerBtn = document.querySelector('.toLogin');
+    const loginArea = document.querySelector('#login-area');
+    const textBox = document.querySelector('.textBox');
+    const registerArea = document.querySelector('#register-area');
+
+    loginBtn.addEventListener('click', ()=> {
+      registerArea.classList.remove('modoLogin')
+      textBox.classList.remove('modoLogin')
+      loginArea.classList.remove('modoLogin')
+      registerArea.classList.add('modoRegistro')
+      textBox.classList.add('modoRegistro')
+      loginArea.classList.add('modoRegistro')
+    })
+    registerBtn.addEventListener('click', ()=> {
+      registerArea.classList.remove('modoRegistro')
+      textBox.classList.remove('modoRegistro')
+      loginArea.classList.remove('modoRegistro')
+      registerArea.classList.add('modoLogin')
+      textBox.classList.add('modoLogin')
+      loginArea.classList.add('modoLogin')
+    })
+  }
+  boxFormAnimation()
+}
+
+controlTelaLogin()
+
+document.getElementById("enviarRegister").addEventListener("click", saveClient);
